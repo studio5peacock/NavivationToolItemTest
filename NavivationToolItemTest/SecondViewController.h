bd03442d54837dfdd22913566356b61f6fad3fbe
studio5peacock <studio5peacock@gmail.com>
2016-06-19 19:43:41 -0400

    ADD socodviewcontroll

---
+++ NavivationToolItemTest/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  NavivationToolItemTest
+//
+//  Created by Genming Zhao on 2016-06-19.
+//  Copyright © 2016 Genming Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
